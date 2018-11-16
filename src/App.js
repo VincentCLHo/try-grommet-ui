dd064bb00ed3405f0743e2c2ad1e919573d53ae8
Vincent Ho <vincent.ho@shoplineapp.com>
2018-11-16 18:39:24 +0800

    with theme

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
 import React, { Component } from 'react';
+import { Grommet, Box} from 'grommet';
 
+const theme = {
+  global: {
+    font: {
+      family: 'Roboto',
+      size: '14px',
+      height: '20px',
+    },
+  },
+};
+const AppBar = (props) => (
+    <Box
+      tag='header'
+      direction='row'
+      align='center'
+      justify='between'
+      background='brand'
+      pad={{ left: 'medium', right: 'small', vertical: 'small' }}
+      elevation='medium'
+      style={{ zIndex: '100' }}
+      {...props}
+    />
+  );
 class App extends Component {
   render() {
     return (
-      <div>
-        123
-      </div>
+      <Grommet theme={theme}>
+        <AppBar>
+          Hello Grommet!
+        </AppBar>
+      </Grommet>
     );
   }
 }
